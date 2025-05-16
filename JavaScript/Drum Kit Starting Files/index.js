862190addb50feb44ced701baa8ecf8214bdd2e5
Neduri Raju <rajneduri123@gmail.com>
2025-05-16 11:17:31 +0530

    updated

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -1,63 +1,60 @@
+// Constructor Function
+function Drum(key, soundPath) {
+  this.key = key;
+  this.sound = soundPath;
+  this.play = function () {
+    new Audio(this.sound).play();
+  };
+}
+
+// Create drum objects
+const drumKit = [
+  new Drum("w", "sounds/tom-1.mp3"),
+  new Drum("a", "sounds/crash.mp3"),
+  new Drum("s", "sounds/tom-2.mp3"),
+  new Drum("d", "sounds/kick-bass.mp3"),
+  new Drum("j", "sounds/tom-3.mp3"),
+  new Drum("k", "sounds/snare.mp3"),
+  new Drum("l", "sounds/tom-4.mp3"),
+];
+
+// Play sound by key
 function playSound(key) {
-  switch (key) {
-    case "w":
-      new Audio("sounds/tom-1.mp3").play();
-      break;
-    case "a":
-      new Audio("sounds/crash.mp3").play();
-      break;
-    case "s":
-      new Audio("sounds/tom-2.mp3").play();
-      break;
-    case "d":
-      new Audio("sounds/kick-bass.mp3").play();
-      break;
-    case "j":
-      new Audio("sounds/tom-3.mp3").play();
-      break;
-    case "k":
-      new Audio("sounds/snare.mp3").play();
-      break;
-    case "l":
-      new Audio("sounds/tom-4.mp3").play();
-      break;
-    default:
-      console.log("Unknown key:", key);
-      return;
-  }
-  buttonAnimation(key);
+  const drum = drumKit.find((d) => d.key === key);
+  if (drum) drum.play();
 }
 
-function buttonAnimation(currentKey) {
-  const activeButton = document.querySelector("." + currentKey);
-  if (!activeButton) return;
-
-  activeButton.classList.add("pressed", "bounce");
-
-  setTimeout(() => {
-    activeButton.classList.remove("pressed", "bounce");
-  }, 300);
-}
-
-// Click event listeners on buttons
-const buttons = document.getElementsByClassName("drum");
+// Button click
+let buttons = document.getElementsByClassName("drum");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    const buttonInnerText = this.innerText.toLowerCase();
-    playSound(buttonInnerText);
+    playSound(this.innerText);
+    animateButton(this.innerText);
   });
 }
 
-// Keyboard event listener
+// Key press
 document.addEventListener("keydown", function (event) {
-  const keyPressed = event.key.toLowerCase();
-  if (keyPressed === "t") {
-    // Play a short tune on pressing 't'
-    const tune = ["w", "a", "s", "d", "j", "k", "l"];
-    for (let i = 0; i < tune.length; i++) {
-      setTimeout(() => playSound(tune[i]), i * 400);
-    }
-  } else {
-    playSound(keyPressed);
+  playSound(event.key);
+  animateButton(event.key);
+
+  // Play tune if "T" is pressed
+  if (event.key.toLowerCase() === "t") {
+    let tune = ["w", "a", "s", "d", "j", "k", "l"];
+    tune.forEach((key, index) => {
+      setTimeout(() => {
+        playSound(key);
+        animateButton(key);
+      }, index * 400);
+    });
   }
 });
+
+// Animation
+function animateButton(currentKey) {
+  const activeButton = document.querySelector("." + currentKey);
+  if (activeButton) {
+    activeButton.classList.add("pressed");
+    setTimeout(() => activeButton.classList.remove("pressed"), 150);
+  }
+}
